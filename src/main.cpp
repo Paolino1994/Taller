c970dcf4427319c7574d8f97bf62e5e9aebc890e
fran <frantguerini@gmail.com>
2018-04-17 12:03:03 -0300

    cambio SDL tutorial por tehkan fiuba cup

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ bool init_SDL()
         }
 
         //Create window
-        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        gWindow = SDL_CreateWindow( "TEHKAN FIUBA CUP", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         if( gWindow == NULL )
         {
             printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
