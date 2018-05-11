383cd3c32227b2a3202db6c966bbb18d3f189303
mantilla <rmantilla02@gmail.com>
2018-05-10 23:24:51 -0300

    se agrega include  #include <algorithm>

--- src/client/World.cpp
+++ src/client/World.cpp
@@ -2,6 +2,7 @@
 #include "BallController.h"
 #include "TeamFactory.h"
 #include "PlayerControllerHuman.h"
+#include <algorithm>
 
 World::World(int width, int height, Texture* background):
     background(background),
