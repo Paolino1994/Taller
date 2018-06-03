535b9520ccd8a6d6d89d049e0c7aa388a0ddc6b0
mantilla <rmantilla02@gmail.com>
2018-06-02 21:38:25 -0300

    limites de los jugadores en la cancha y en la minicamera

--- src/client/Camera.cpp
+++ src/client/Camera.cpp
@@ -98,7 +98,7 @@ void Camera::render(World& world){
 	for (std::pair<const Player_ID, Player>& pair : players)
 		{
 			Player& player = pair.second;
-			player.renderMiniMap(miniFieldRect->getPosX() + 5, miniFieldRect->getPosY() + 5);
+			player.renderMiniMap(miniFieldRect->getPosX() + 5, miniFieldRect->getPosY() + 10);
 		}
 
 
@@ -106,7 +106,7 @@ void Camera::render(World& world){
 	int screen_y = world.getBall().getY() - this->y;
 	Log::get_instance()->info("X: " + std::to_string(screen_x) + " Y: " + std::to_string(screen_y));
 	world.getBall().render(screen_x, screen_y + YAML::MINIMAP_HEIGHT + 20);
-	world.getBall().renderMiniMap(miniFieldRect->getPosX(), miniFieldRect->getPosY() + 5);
+	world.getBall().renderMiniMap(miniFieldRect->getPosX(), miniFieldRect->getPosY() + 10);
 }
 
 void Camera::renderMiniCamera(){
@@ -119,17 +119,17 @@ void Camera::renderMiniCamera(){
 	// 250 / WORLD_WIDTH  = 0.147059
 	// 150 / WORLD_HEIGHT = 0.142857
 
-	if (followed_y < YAML::MINIMAP_HEIGHT - 50) pos_y = 0;
+	if (followed_y < YAML::MINIMAP_HEIGHT - 10) pos_y = 0;
 		else {
-			if (followed_y > 750) pos_y = 90;
-				else pos_y = miniFieldRect->getPosY() - 20 + followed_y * (0.142857);
+			if (followed_y > 750) pos_y = 80;
+				else pos_y = miniFieldRect->getPosY() - 25 + followed_y * (0.142857);
 		}
 
 
 	if (followed_x < 185) pos_x = miniFieldRect->getPosX();
 		else{
 			if(followed_x > 1484) pos_x = miniFieldRect->getPosX() + 190;
-				else pos_x = miniFieldRect->getPosX() - 25 + followed_x * (0.147059);
+				else pos_x = miniFieldRect->getPosX() - 30 + followed_x * (0.147059);
 		}
 
 	miniCameraRect->render(pos_x, pos_y);
