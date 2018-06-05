c1c9cb9a12a6e9f5a39b3b14c94ce453031e7241
mantilla <rmantilla02@gmail.com>
2018-06-04 21:11:42 -0300

    agrego las condiciones de gol para TEAM:HOME Y TEAM:AWAY

--- src/server/BallPassesEndLineSystem.cpp
+++ src/server/BallPassesEndLineSystem.cpp
@@ -21,16 +21,18 @@ void BallPassesEndLineSystem::process(double dt)
 	if (GameManager::get_instance().isBallInPlay()) {
 		BallModel& ballModel = world.getBall().getModel();
 
-		int leftEndLineX = 60;
-		int goalBottomPost = 420;
-		int goalTopPost = 620;
-		int postWidth = 30;
-		//int rigthEndLineX = 30;
+		int leftEndLineX = 50;
+		int goalBottomPost = 347;
+		int goalTopPost = 550;
+		int postWidth = 20;
+		int rigthEndLineX = 1628;
+
 
 		int x = ballModel.getX();
 		int y = ballModel.getY();
 		// double z = ballModel.getZ();
 
+		// condiciones de gol para Team::AWAY
 		if (x < leftEndLineX)
 		{
 			if (y > goalBottomPost - postWidth && y < goalTopPost + postWidth)  // Tambien podemos poner la validacion en eje z para el travesanio
@@ -40,7 +42,7 @@ void BallPassesEndLineSystem::process(double dt)
 					std::cout << "GOOOL" << std::endl;
 					//TEMP -> TODO: ojo que el que metio el gol quizas no la pateo, y entro caminando con la pelota por el arco
 					//TEMP -> TODO: chequear gol en base al arco y posicion del equipo
-					Team goalTeam = this->lastPlayerThatKickedTheBall != nullptr ? this->lastPlayerThatKickedTheBall->getTeam() : Team::HOME;
+					Team goalTeam = this->lastPlayerThatKickedTheBall != nullptr ? this->lastPlayerThatKickedTheBall->getTeam() : Team::AWAY;
 					EventQueue::get().push(std::make_shared<GoalEvent>(this->lastPlayerThatKickedTheBall, goalTeam));
 				}
 				else {
@@ -52,6 +54,26 @@ void BallPassesEndLineSystem::process(double dt)
 				std::cout << "Salio por el costado izquierdo" << std::endl;
 			}
 		}
+
+		// condiciones de gol para Team::HOME
+		if (x > rigthEndLineX) {
+			if (y > goalBottomPost - postWidth && y < goalTopPost + postWidth) // Tambien podemos poner la validacion en eje z para el travesanio
+					{
+				if (y > goalBottomPost && y < goalTopPost) {
+					std::cout << "GOOOL" << std::endl;
+					// Aca estoy probando el GameManager, no es para nada definitivo que sea asi
+//					GameManager::get_instance()->addGoal(Team::HOME); // Obvio que no es asi porque falta la logica del cambio de lado
+					Team goalTeam = this->lastPlayerThatKickedTheBall != nullptr ? this->lastPlayerThatKickedTheBall->getTeam() : Team::HOME;
+					EventQueue::get().push(std::make_shared<GoalEvent>(this->lastPlayerThatKickedTheBall, goalTeam));
+				} else {
+					std::cout << "PALO" << std::endl;
+					ballModel.setVelX(-ballModel.getVelX()); // Hago rebotar la pelota en el palo, le cambio el angulo en x
+				}
+			} else {
+				std::cout << "Salio por el costado derecho" << std::endl;
+			}
+		}
+
 	}
 }
 
